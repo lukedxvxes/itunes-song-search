5f5713b3d4d40dfaf56b2edf0cf14ca693bb3a70
Luke Davies <lukedaviesweb+1@gmail.com>
2023-07-25 13:43:21 +0700

    basic UI setup - mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,54 @@
-import { Button } from '@/components/ui/button';
-import './global.css';
+import { Input } from '@/components/ui/input';
 import './app.scss';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Progress } from '@/components/ui/progress';
+import {
+  PlayIcon,
+  PauseIcon,
+  TrackPreviousIcon,
+  TrackNextIcon,
+  BarChartIcon,
+} from '@radix-ui/react-icons';
+
 function App() {
   return (
     <div id="song-search-app">
-      <h1>Search bar</h1>
+      <div className="mb-4">
+        <Input type="text" placeholder="Search by artist" />
+      </div>
 
-      <div>
-        <h1>song results list</h1>
+      <div className="mb-4">
+        <ScrollArea className="h-[200px]  rounded-md border p-4">
+          <ul>
+            <li className="flex items-center justify-between">
+              Song <BarChartIcon />
+            </li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+            <li>Song</li>
+          </ul>
+        </ScrollArea>
       </div>
 
       <div>
-        <h1>player controls</h1>
+        <Progress value={33} />
+        <div className="flex items-center justify-around">
+          <TrackPreviousIcon />
+          <PlayIcon />
+          <PauseIcon />
+          <TrackNextIcon />
+        </div>
       </div>
-      <Button>Click me</Button>
     </div>
   );
 }
