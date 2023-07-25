2923c753211c2060bcebbeb308cdb77b4e8af7ac
Luke Davies <lukedaviesweb+1@gmail.com>
2023-07-25 13:30:34 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button';
+import './global.css';
+import './app.scss';
+function App() {
+  return (
+    <div id="song-search-app">
+      <h1>Search bar</h1>
+
+      <div>
+        <h1>song results list</h1>
+      </div>
+
+      <div>
+        <h1>player controls</h1>
+      </div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
