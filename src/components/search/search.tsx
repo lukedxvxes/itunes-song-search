c801beda0140426122451675f3c515229c02b6ef
Luke Davies <lukedaviesweb+1@gmail.com>
2023-07-25 16:43:05 +0700

    wip - add to query param approach

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,9 +1,30 @@
 import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { FormEvent } from 'react';
 
 export const Search = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.target as HTMLFormElement;
+    const formData = new FormData(target);
+    const artist = formData.get('artist');
+
+    console.log({ artist });
+  };
   return (
     <div className="mb-4">
-      <Input type="text" placeholder="Search by artist" />
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className="flex w-full items-center space-x-2 "
+      >
+        <Input
+          className="flex-1"
+          type="text"
+          placeholder="Search by artist"
+          name="artist"
+        />
+        <Button type="submit">Search</Button>
+      </form>
     </div>
   );
 };
